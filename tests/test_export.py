70fc2a9803eb50413792c87e63b807744c9fe82d
Fabian Eulitz <dev@sustineo.de>
2022-05-22 15:33:04 +0200

    Implemented a non-interactive way to call the export function (WIP)
    - Added more test cases
    - Fixed an issue with the --interactive flag

--- tests/test_export.py
+++ tests/test_export.py
@@ -1,10 +1,19 @@
-from typer.testing import CliRunner
+# Built-in modules
+from pathlib import Path
 
+# PyPi modules
+from typer.testing import CliRunner
+from dotenv import load_dotenv
+
+# Local modules
 from steam_market_history.main import app
 
+load_dotenv()  # take environment variables from .env.
 runner = CliRunner()
 
 
-def test_export_default():
-    # TODO: Find a way to programmatically authenticate to steam
-    assert True
+# def test_export_default():
+#     result = runner.invoke(app, ["export", "--non-interactive", "--csv", "--html"])
+#     assert result.exit_code == 0
+#     assert Path("./steam-market-history.csv").is_file()
+#     assert Path("./steam-market-history.html").is_file()
